428d42bb963bdfefa0bb0f3fced56192e4675753
Prince Pal <princepal262501@gmail.com>
2025-08-07 15:33:50 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,77 @@
+let cityName = document.querySelector(".weather_city");
+let dateTime = document.querySelector(".weather_date_time");
+let w_forecast = document.querySelector(".weather_forecast");
+let w_temperature = document.querySelector(".weather_temperature");
+let w_icon = document.querySelector(".weather_icon");
+let w_minTEm = document.querySelector(".weather_min");
+let w_maxTem = document.querySelector(".weather_max");
+let w_feelsLike = document.querySelector(".weather_feelsLike");
+let w_humidity = document.querySelector(".weather_humidity");
+let w_wind = document.querySelector(".weather_wind");
+let w_pressure = document.querySelector(".weather_pressure");
+let citySearch = document.querySelector(".weather_search");
+let inputName = document.querySelector(".city_name");
+
+const getCountryName = (code) => {
+  return new Intl.DisplayNames([code], { type: "region" }).of(code);
+};
+
+const getDateTime = (dt) => {
+  const currDate = new Date(dt * 1000);
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  const formatter = new Intl.DateTimeFormat("en-US", options);
+
+  return formatter.format(currDate);
+};
+
+let city = "Bhimtal";
+
+// search functionality
+citySearch.addEventListener("submit",( (e)=>{
+e.preventDefault();
+
+
+let inputValue = inputName.value;
+city = inputValue;
+getWeatherData();
+inputName.value = ""
+  
+} ))
+
+
+const getWeatherData = async () => {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=59f5c8a3724d4b9661c862b585647cc8
+ `;
+  try {
+    const response = await fetch(weatherUrl);
+    const data = await response.json();
+    console.log(data);
+    const { main, name, weather, wind, sys, dt } = data;
+
+    cityName.innerHTML = `${name}, ${getCountryName(sys.country)}`;
+
+    dateTime.innerHTML = getDateTime(dt);
+    w_forecast.innerHTML = weather[0].main;
+    w_icon.innerHTML = ` <img src="https://openweathermap.org/img/wn/${weather[0].icon}@2x.png" >`; 
+ 
+    w_temperature.innerHTML = `${ main.temp}&#176`;
+    w_minTEm.innerHTML = `Min: ${main.temp_min.toFixed()}&#176`;
+    w_maxTem.innerHTML = `Max: ${main.temp_max.toFixed()}&#176`;
+    w_feelsLike.innerHTML = `${main.feels_like}&#176`;
+    w_humidity.innerHTML = `${main.humidity}&#176`;
+    w_pressure.innerHTML = `${main.pressure}&#176`;
+    w_wind.innerHTML = `${wind.speed}&#176`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+window.addEventListener("load", getWeatherData);
---
+++ steps.md
@@ -0,0 +1,33 @@
+# API Project Steps 
+
+## Step 1 : Select DOM Elements
+
+ - Select DOM Elements for displaying weather information using appopriate query selectors.
+
+ ## Step 2 : Fetch Weather Data
+  - Define a functions to fetch weather data from the OpenWeatherMap API using the "fetch" API.
+  - Extract relevant data from the API response such as temperature , humidity , wind speed etc.
+
+  ## Step 3 : Review API Data
+
+- Review the strucutre of the API response to understand the data available for display.
+
+ ## Step 4 : Display City Name
+
+ - Display the city name along with its country code using the provided API data.
+
+ ## Step 5 : Format Date and Time
+ 
+ - Format the timestamp provided by the API into a human - readable date and time format .
+
+ ## Step 6 : Display Additonal Weather Details
+
+ - Display additonal weather details sucha as temperature, minimum , and maximum temperature , feels like temperature , humidity , wind speed , and pressure . 
+
+ ## Step 7 : Handle City Search
+
+ - Implement Functionality to allow users search
+ for weather information of different cities .
+ - Add event listeners to handle city search input and trigger the weather data fetching function 
+ accordingly . 
+ 
